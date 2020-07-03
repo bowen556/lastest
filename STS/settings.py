692b1872601e81f90ff6c304666d8b4a9f6772c3
bowen111 <43743474+Zhangbowen40333150@users.noreply.github.com>
2020-07-03 18:44:32 +0100

    Initial

--- STS/settings.py
+++ STS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from os import path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,10 +126,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+
+
+
+
+
 STATIC_URL = '/static/'
-
 MEDIA_URL = '/images/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
